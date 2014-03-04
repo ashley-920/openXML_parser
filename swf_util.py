c0078c4599aad6cff8b5956b167af0adf7055eaa
Ashley S <ashley-920@hotmail.com>
2014-03-04 18:47:41 +0800

    modify extract_file function

--- swf_util.py
+++ swf_util.py
@@ -1,4 +1,4 @@
-import os
+import os, subprocess as sub
 
 file_path="C:\\Users\\Ash\\Desktop\\swf\\activeX1.swf"
 des_text_path="C:\\Users\\Ash\\Desktop\\action_result.txt"
@@ -9,11 +9,19 @@ mod_dir= "\\prog\\swftools\\swfdump.exe"
 
 def extract_actionscript(file_path,des_text_path):
 	#print os.path.dirname(__file__)
+	# print file_path
+	# print des_text_path
 	prog_swftools = os.path.dirname(__file__)+mod_dir
 	#print prog_swftools
-	test_command=prog_swftools+" -a "+file_path+" > "+des_text_path
-	print 'actionscript file location: %s' % (des_text_path)
-	os.popen(test_command)
+	# des_text_path=des_text_path.replace(" ", "_")
+	test_command="\""+prog_swftools+"\" -a \""+file_path+"\" > \""+des_text_path+"\""
+	# test_command="\""+prog_swftools+"\" -a \""+file_path+"\""
+
+	# print test_command
+	print 'ActionScript file location:'
+	print  '*%s' % (des_text_path)
+	sub.Popen(test_command,shell=True, stdout=sub.PIPE, stderr=sub.STDOUT)
+	# os.system(str(test_command))
 
 if __name__ == '__main__':
 	extract_actionscript(file_path, des_text_path)
